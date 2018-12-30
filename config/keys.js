887a30a8a1b0a14aaa98d88b5abc357b395daba5
Grace Park <parkg748@newschool.edu>
2018-12-30 14:02:24 -0800

    add jwt and validator authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://guest:starwars1@ds145584.mlab.com:45584/devconnector-1'
-}
+  mongoURI: 'mongodb://guest:starwars1@ds145584.mlab.com:45584/devconnector-1',
+  secretOrKey: 'secret'
+};
